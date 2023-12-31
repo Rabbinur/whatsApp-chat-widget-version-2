063a32c824213e22481b9160359e42a7f7d19940
Md. Rabbinur Muktar <59724876+Rabbinur@users.noreply.github.com>
2023-12-31 16:26:46 +0600

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import { WhatsAppWidget } from "react-whatsapp-widget";
+import "react-whatsapp-widget/dist/index.css";
+
+
+
+
+
+const app=()=>{
+  return ( <div>
+          <WhatsAppWidget CompanyIcon={CompanyIcon}  phoneNumber="+8801318222631" />
+        </div>)}
